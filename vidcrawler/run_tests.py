1f4a839e48e1ec9d1fa15478441d79b1a638a51c
zackees <z@zackees.com>
2022-05-15 23:56:15 -0700

    nit, make testing return value

--- vidcrawler/run_tests.py
+++ vidcrawler/run_tests.py
@@ -2,8 +2,10 @@
     Trampoline to get tests.
 """
 
+
 if __name__ == "__main__":
     import os
+    import sys
 
     os.chdir(os.path.dirname(__file__))
-    os.system("pytest testing")
+    sys.exit(os.system("pytest testing"))
