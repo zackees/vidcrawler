1720b6eccf4bc83c4bcce8c41b04d63f227b55da
zackees <z@zackees.com>
2022-05-15 20:08:47 -0700

    refactor testing

--- vidcrawler/run_tests.py
+++ vidcrawler/run_tests.py
@@ -1,3 +1,8 @@
-import os;
-os.chdir(os.path.dirname(__file__))
-os.system("python -m unittest discover tests")
+"""
+    Trampoline to get tests.
+"""
+
+if __name__ == "__main__":
+    import os
+    os.chdir(os.path.dirname(__file__))
+    os.system("python -m unittest discover testing")
--- vidcrawler/testing/test_basic.py
+++ vidcrawler/testing/test_basic.py
@@ -10,9 +10,10 @@ import unittest
 class DatabaseTester(unittest.TestCase):
     """Tester for the Full Search Text Database"""
 
-    def test(self) -> None:
+    def test_sanity(self) -> None:  # pylint: disable=no-self-use
         """Test the full text search database."""
         return
+
 
 if __name__ == "__main__":
     unittest.main()
