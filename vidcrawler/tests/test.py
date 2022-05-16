1c1c59b1a739b5521a4382a8a88a169e8b988604
zackees <z@zackees.com>
2022-05-15 19:36:27 -0700

    adds testing

---
+++ vidcrawler/tests/test.py
@@ -0,0 +1,18 @@
+"""
+    Stub test
+"""
+
+# pylint: disable=invalid-name,R0801
+
+import unittest
+
+
+class DatabaseTester(unittest.TestCase):
+    """Tester for the Full Search Text Database"""
+
+    def test(self) -> None:
+        """Test the full text search database."""
+        return
+
+if __name__ == "__main__":
+    unittest.main()
