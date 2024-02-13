646de753e720eb6299e486cae7c040f14f5cb526
zackees <z@zackees.com>
2024-02-13 10:27:59 -0800

    adds playwright

---
+++ vidcrawler/testing/test_playwright.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+from playwright.sync_api import sync_playwright
+
+IS_GITHUB_RUNNER = os.environ.get("GITHUB_ACTIONS") == "true"
+
+
+class PlaywrightTester(unittest.TestCase):
+    """Tests basic functionality of Playwright."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.playwright = sync_playwright().start()
+        # Launch a headed browser by setting headless to False
+        cls.browser = cls.playwright.chromium.launch(headless=IS_GITHUB_RUNNER)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.close()
+        cls.playwright.stop()
+
+    def setUp(self):
+        self.page = self.browser.new_page()
+
+    def tearDown(self):
+        self.page.close()
+
+    def test_has_title(self):
+        """Simple test to verify the title of a page."""
+        # Go to page google.com
+        self.page.goto("https://www.google.com")
+        # Get page title
+        title = self.page.title()
+        # Verify the title
+        self.assertEqual(title, "Google")
+
+
+if __name__ == "__main__":
+    unittest.main()
