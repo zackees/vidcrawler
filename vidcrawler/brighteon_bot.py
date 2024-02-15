683fe8f540c11e937f72da67f9ef195caab4c5ed
zackees <z@zackees.com>
2024-02-14 16:28:06 -0800

    refactor playwright

--- vidcrawler/brighteon_bot.py
+++ vidcrawler/brighteon_bot.py
@@ -1,3 +1,5 @@
+# pylint: disable=too-many-locals
+
 """
 Scrapes the brighteon website for video urls and downloads them.
 """
@@ -6,57 +8,31 @@ import argparse
 import os
 import sys
 import warnings
-from contextlib import contextmanager
-from typing import Generator
 
-from filelock import FileLock
-from playwright.sync_api import Browser, Page, sync_playwright
+from playwright.sync_api import Page
 
 from vidcrawler.library import Library, VidEntry
+from vidcrawler.playwright_launcher import launch_playwright, set_headless
 
 BASE_URL = "https://www.brighteon.com"
 
 INSTALLED = False
 
 
-def install_playwright() -> None:
-    """Install Playwright."""
-    install_lock = os.path.join(os.getcwd(), "playwright.lock")
-    with FileLock(install_lock):
-        global INSTALLED  # pylint: disable=global-statement
-        if INSTALLED:
-            return
-        rtn = os.system("playwright install")
-        if rtn != 0:
-            raise OSError("Failed to install Playwright.")
-        INSTALLED = True
-
-
-@contextmanager
-def launch_playwright() -> Generator[tuple[Page, Browser], None, None]:
-    """Playwright context manager."""
-    install_playwright()
-    with sync_playwright() as context:
-        is_github_runner = os.environ.get("GITHUB_ACTIONS") == "true"
-        browser = context.chromium.launch(headless=is_github_runner)
-        page = browser.new_page()
-        try:
-            yield (page, browser)
-        finally:
-            page.close()
-            browser.close()
-
-
-def get_vids(page: Page, channel_url: str, page_num: int) -> list[VidEntry]:
+def _fetch_vid_infos(page: Page, channel_url: str, page_num: int) -> list[VidEntry]:
     """Get the urls from a channel page. Throws exception when page not found."""
     # From: https://www.brighteon.com/channels/hrreport
     # To: https://www.brighteon.com/channels/hrreport/videos?page=1
     url = f"{channel_url}/videos?page={page_num}"
-    page.goto(url)
+    print(f"Fetching video list from {url}")
+    response = page.goto(url)
+    assert response
+    if response.status != 200:
+        raise ValueError(f"Failed to fetch {url}, status: {response.status}")
     # get all div class="post" objects
     posts = page.query_selector_all("div.post")
     # get the first one
-    urls: list[VidEntry] = []
+    vids: list[VidEntry] = []
     for post in posts:
         try:
             link = post.query_selector("a")
@@ -67,18 +43,20 @@ def get_vids(page: Page, channel_url: str, page_num: int) -> list[VidEntry]:
             assert title
             title_text = title.inner_text().strip()
             href = BASE_URL + href
-            urls.append(VidEntry(title=title_text, url=href))
+            vids.append(VidEntry(title=title_text, url=href))
         except Exception as e:  # pylint: disable=broad-except
             warnings.warn(f"Failed to get url: {e}")
-    return urls
+    print(f"Found {len(vids)} videos.")
+    return vids
 
 
-def update_library(outdir: str, channel_name: str, limit: int = -1) -> Library:
+def _update_library(outdir: str, channel_name: str, full_scan: bool, limit: int = -1) -> Library:
     """Simple test to verify the title of a page."""
     channel_url = f"https://www.brighteon.com/channels/{channel_name}"
-    library_json = os.path.join(outdir, channel_name, "brighteon", "library.json")
+    library_json = os.path.join(outdir, "library.json")
     library = Library(library_json)
     count = 0
+    stored_vids: list[VidEntry] = library.load()
     with launch_playwright() as (page, _):
         # Determine whether to run headless based on the environment variable
         urls: list[VidEntry] = []
@@ -89,7 +67,13 @@ def update_library(outdir: str, channel_name: str, limit: int = -1) -> Library:
                     break
             count += 1
             try:
-                new_urls: list[VidEntry] = get_vids(page, channel_url, page_num)
+                new_urls: list[VidEntry] = _fetch_vid_infos(page, channel_url, page_num)
+                set_new_urls = set(new_urls)
+                set_stored_vids = set(stored_vids)
+                # if the new urls are fully contained in the stored vids, then we are done
+                if not full_scan and (set_new_urls <= set_stored_vids):
+                    warnings.warn("All the new videos are already in the library... halting scan.")
+                    break
                 page_num += 1
                 urls += new_urls
             except Exception as e:  # pylint: disable=broad-except
@@ -102,13 +86,14 @@ def update_library(outdir: str, channel_name: str, limit: int = -1) -> Library:
 
 def main() -> int:
     """Main function."""
-    parser = argparse.ArgumentParser("brighteon-pull")
+    parser = argparse.ArgumentParser("brighteon-pull-channel pulls a channel from brighteon.com.")
     parser.add_argument(
-        "channel",
+        "--channel-name",
         type=str,
         help="URL slug of the channel, example: hrreport",
+        required=True,
     )
-    parser.add_argument("basedir", type=str, help="Base directory", default=".")
+    parser.add_argument("--output", type=str, help="Output directory", required=True)
     parser.add_argument(
         "--limit-downloads",
         type=int,
@@ -118,13 +103,17 @@ def main() -> int:
         "--skip-download",
         action="store_true",
     )
+    set_headless(True)
+    # full-scan
+    parser.add_argument("--full-scan", action="store_true", help="Scan the entire channel, not just the new videos.")
     args = parser.parse_args()
-    basedir = args.basedir
-    channel = args.channel
+    outdir = args.output
+    channel = args.channel_name
     download_limit = args.limit_downloads
     skip_download = args.skip_download
+    full_scan = args.full_scan
 
-    library = update_library(basedir, channel, limit=-1)
+    library = _update_library(outdir, channel, full_scan=full_scan, limit=download_limit)
     if not skip_download:
         library.download_missing(download_limit)
     return 0
@@ -132,8 +121,10 @@ def main() -> int:
 
 def unit_test(limit=-1) -> int:
     """Run the tests."""
+    sys.argv.append("--channel-name")
     sys.argv.append("hrreport")
-    sys.argv.append("tmp")
+    sys.argv.append("--output")
+    sys.argv.append("tmp2")
     sys.argv.append("--limit-downloads")
     sys.argv.append(str(limit))
     main()
---
+++ vidcrawler/playwright_launcher.py
@@ -0,0 +1,47 @@
+"""
+Launches playwright and sets up all the defaults.
+"""
+
+import os
+from contextlib import contextmanager
+from typing import Generator
+
+from filelock import FileLock
+from playwright.sync_api import Browser, Page, sync_playwright
+
+INSTALLED = False
+HEADLESS = True
+
+
+def set_headless(headless: bool) -> None:
+    """Set headless."""
+    global HEADLESS  # pylint: disable=global-statement
+    HEADLESS = headless
+
+
+def install_playwright() -> None:
+    """Install Playwright."""
+    install_lock = os.path.join(os.getcwd(), "playwright.lock")
+    with FileLock(install_lock):
+        global INSTALLED  # pylint: disable=global-statement
+        if INSTALLED:
+            return
+        rtn = os.system("playwright install")
+        if rtn != 0:
+            raise OSError("Failed to install Playwright.")
+        INSTALLED = True
+
+
+@contextmanager
+def launch_playwright() -> Generator[tuple[Page, Browser], None, None]:
+    """Playwright context manager."""
+    install_playwright()
+    with sync_playwright() as context:
+        headless = HEADLESS or os.environ.get("GITHUB_ACTIONS") == "true"
+        browser = context.chromium.launch(headless=headless)
+        page = browser.new_page()
+        try:
+            yield (page, browser)
+        finally:
+            page.close()
+            browser.close()
