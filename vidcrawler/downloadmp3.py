1e48ad7e70261d4b89b787afa6ed3c7596370ffb
zackees <z@zackees.com>
2024-02-20 00:48:59 -0800

    Update docker-run-cmd to version 1.0.5 and vidcrawler to 1.0.19

--- vidcrawler/downloadmp3.py
+++ vidcrawler/downloadmp3.py
@@ -1,10 +1,14 @@
+"""
+Download a youtube video as an mp3.
+"""
+
 import os
 import shutil
 import subprocess
 import tempfile
 import warnings
 
-from docker_run_cmd.run import main as docker_run_cmd
+from docker_run_cmd.api import docker_run
 from static_ffmpeg import add_paths
 
 
@@ -36,41 +40,21 @@ def yt_dlp_download_mp3(url: str, outmp3: str) -> None:
                 continue
         warnings.warn(f"Failed all attempts to download {url} as mp3.")
 
-# This is how we call the containerized version of yt-dlp
-#def unit_test() -> None:
-#    dockerfile = 'src/Dockerfile'  # Replace with your actual Dockerfile path
-#    image_name = 'docker-yt-dlp'
-#    container_name = 'container-docker-yt-dlp'
-#    working_dir = 'src'  # Replace with your container's working directory
-#    host_volume = '.'  # Replace with your host volume
-#    container_volume = '/host_dir'  # Replace with your container volume
-#    cmd_args = ['--version']  # Replace with your command arguments
-#
-#    # Convert paths and run main process
-#    dockerfile = to_abs_path(dockerfile)
-#    working_dir = to_abs_path(working_dir)
-#    main(dockerfile,
-#         image_name,
-#         container_name,
-#         host_volume,
-#         container_volume,
-#         cmd_args,
-#         working_dir)
 
 def docker_yt_dlp_download_mp3(url: str, outmp3: str) -> None:
     """Download the youtube video as an mp3."""
     here = os.path.abspath(os.path.dirname(__file__))
     dockerfile = os.path.join(here, "Dockerfile")
     dockerfile = os.path.abspath(dockerfile)
-    assert  os.path.exists(dockerfile), f"dockerfile {dockerfile} does not exist"
-    image_name = 'docker-vidcrawler-yt-dlp'
-    container_name = 'container-vidcrawler-docker-yt-dlp'
+    assert os.path.exists(dockerfile), f"dockerfile {dockerfile} does not exist"
     with tempfile.TemporaryDirectory() as temp_dir:
         # working_dir = here
-        host_volume = temp_dir
-        container_volume = '/host_dir'
         cmd_args = [url, "--extract-audio", "--audio-format", "mp3", "--output", "/host_dir/temp.mp3", "--update", "--no-geo-bypass"]
-        docker_run_cmd(dockerfile, image_name, container_name, host_volume, container_volume, cmd_args, host_volume)
+        # def docker_run(
+        #  name: str, dockerfile_or_url: str, cwd: Path, cmd_list: list[str]
+        # ) -> int:
+        # docker_run(dockerfile, image_name, container_name, host_volume, container_volume, cmd_args, host_volume)\
+        docker_run(name="yt-dlp", dockerfile_or_url=dockerfile, cwd=temp_dir, cmd_list=cmd_args)
         shutil.copy(os.path.join(temp_dir, "temp.mp3"), outmp3)
 
 
--- vidcrawler/version.py
+++ vidcrawler/version.py
@@ -1,3 +1,3 @@
 """VERSION FILE"""
 
-VERSION = "1.0.18"
+VERSION = "1.0.19"
